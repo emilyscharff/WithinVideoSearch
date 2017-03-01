e10a4743a30f8459d79278ece642b403bb017329
Emily Scharff <emilyscharff@berkeley.edu>
2017-03-01 15:10:43 -0800

    background

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
-chrome.tabs.onUpdated.addListener(checkYoutube);
 
-function showIntercomAction(tabId, changeInfo, tab) {
-  if (tab.url.indexOf('youtube.com') > -1) {
-    // Show icon for page action in the current tab.
-    chrome.pageAction.show(tabId);
-  }
-};
+//
+function search(text) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  		chrome.tabs.sendMessage(tabs[0].id, {greeting: text});
+});
+	alert('You just typed "' + text + '"');
+}
+
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+	search
+ );
