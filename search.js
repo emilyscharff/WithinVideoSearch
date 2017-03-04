d8ca762f572259d16014bea95ff51826d8a67b94
Emily Scharff <emilyscharff@berkeley.edu>
2017-03-03 16:59:06 -0800

    works for english english subtitles, need to refresh for every new video

--- search.js
+++ search.js
@@ -1,3 +1,7 @@
+// var jq = document.createElement('script');
+// jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+// document.getElementsByTagName('head')[0].appendChild(jq);
+
 
 console.log("Hello, world!");
 var $x;
@@ -5,12 +9,17 @@ var $x;
 var ready = false;
 var serializer = new XMLSerializer();
 var video = $("video")[0];
-console.log(video)
-var vidPlayer = document.getElementById('#movie_player')
-console.log(vidPlayer)
-console.log(vidPlayer.getAudioTrack);
-var xmlLink = vidPlayer.getAudioTrack().captionTracks[0]["H"];
+// var vidPlayer = $('#movie_player')[0];
+// debugger;
+
+// console.log(vidPlayer)
+// console.log(vidPlayer.getAudioTrack);
+// var xmlLink = vidPlayer.getAudioTrack().captionTracks[0]["H"];
+var video_id = video.baseURI.split("?v=")[1]
+var xmlLink = "https://www.youtube.com/api/timedtext?v=" + video_id + "&lang=en"
+debugger;
 // var xmlLink = "https://www.youtube.com/api/timedtext?v=6g4u0tsG-aA&key=yttt1&caps=-lyr&expire=1488586154&sparams=caps%2Cv%2Cexpire&hl=en_US&signature=DDE4B99A1720404F02DEE7DA9B4B7529C359E24E.D47BB97D1DA13871A43A159DBD9595C59580207E&lang=en"
+
 var parser = new DOMParser();
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
@@ -49,8 +58,6 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
     if (ready) {
-    console.log("listening");
-    console.log(request.msg)
 
 
 
